ed135959f4aee13ddd0ffbc108a64a3c66e83a2c
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-02-01 21:09:47 +0300

    Make sure the app is running

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
+
+app.get('/', (req, res) => {
+  res.send('api running');
+});
