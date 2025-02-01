dae8afe6385299e15ebae0b95a9438a124c1d646
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-02-01 21:07:25 +0300

    Initialize the project

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+// const { Pool } = require('pg');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
