697244f0084049d0b4932a950aacf5f48b0ad020
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2025-02-01 21:33:36 +0300

    Implement get flashcards api

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
-// const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
+const { Pool } = require('pg');
+const config = require('./app/config');
+
+const pool = new Pool({
+  host: config.HOST,
+  user: config.USER,
+  password: config.PASSWORD,
+  database: config.DB,
+});
 
 app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
 
 app.get('/', (req, res) => {
   res.send('api running');
 });
+
+app.get('/flashcards/:subject/:userId', (req, res) => {
+  const { subject, userId } = req.params;
+  pool.query('SELECT * FROM flashcards WHERE subject = ? AND user_id = ?', [subject, userId], (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+app.listen(port, () => console.log(`API listening on port ${port}`));
